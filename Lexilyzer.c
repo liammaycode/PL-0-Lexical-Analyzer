58bb047adce62fe7e5fa7afc14d3427a66389c8f
Liam May <liam_may@knights.ucf.edu>
2020-02-17 16:38:52 -0500

    Update Lexilyzer.c

--- Lexilyzer.c
+++ Lexilyzer.c
@@ -29,21 +29,10 @@ typedef struct lexemes
   char *lexeme;
 }lexeme;
 
-lexeme *createLexeme(token_type t, char *str);
 void trim(char *str);
-int parse(char *str, lexeme list[]);
-bool symboltable(char symbol);
+void parse(char *code);
+bool isReserved(char *str);
 
-lexeme *createLexeme(token_type t, char *str)
-{
-	lexeme *l = malloc(1 * sizeof(lexeme));
-	l->type = t;
-  l->lexeme = str;
-	return l;
-}
-
-// gets rid of comments by overwriting all character within the comments and the
-// comment markers themselves
 void trim(char *str)
 {
   int lp = 0, rp, diff, i, len = strlen(str);
@@ -52,6 +41,7 @@ void trim(char *str)
 
   while (str[lp] != '\0')
   {
+
     if (str[lp] == '/' && str[lp + 1] == '*')
     {
       rp = lp + 2;
@@ -70,415 +60,166 @@ void trim(char *str)
   return;
 }
 
-// adds lexemes to table and checks for errors
-int parse(char *str, lexeme list[])
+void parse(char *code)
 {
-  int i = 0, j = 0, k = 0, diff, x, length = 0, a, b;
+  lexeme list[MAX_CODE_LENGTH];
+  lexeme *lptr;
+  int lp = 0, rp, length, i;
+  char buffer[MAX_CODE_LENGTH];
+
+  while (code[lp] != '\0')
+  {
+    if (isalpha(code[lp]))
+    {
+      rp = lp;
+      // capturing length of substring
+      while (isalpha(code[lp]) || isdigit(code[lp]))
+      {
+        rp++;
+      }
+      length = rp - lp;
+
+      // checking for ident length error
+      if (length > MAX_IDENT_LENGTH)
+      {
+        printf("Err: ident length too long\n");
+        return;
+      }
+
+      // creating substring
+      for (i = 0; i < length; i++)
+      {
+        buffer[i] = code[lp + i];
+      }
+
+      if (isReserved(buffer))
+      {
+        
+      }
+    }
+    else
+    {
+      // err
+    }
+  }
+
+}
+
+// return true if the string is a reserved keyword and false otherwise
+bool isReserved(char *str)
+{
   // Table of reserved word names
   char reserved[14][9] = { "const", "var", "procedure", "call", "begin", "end",
                            "if", "then", "else", "while", "do", "read", "write",
                            "odd" };
-  char buffer[MAX_CODE_LENGTH];
-  lexeme *l;
-  while(str[i] != '\0')
+
+  if (str[0] == 'b')
   {
-    // const
-    if(str[i] == 'c' && str[i+1] == 'o' && str[i+2]=='n' && str[i+3] == 's' && str[i+4] == 't')
+    if (strcmp(reserved[4], str) == 0)
     {
-      l = createLexeme(constsym, "const");
-      list[j++] = *l;
-      // loop through whitespace
-      i += 5;
-      while (!isspace(str[i++]));
-      // capture identifier (and make sure it's valid)
-      if (isalpha(str[i]))
-      {
-        // capture identifier as substring and put in list
-        i = k;
-        while (!symboltable(str[k++]))
-        {
-          if (!isspace(str[k]))
-          {
-            length++;
-          }
-        }
-        if ((length) > MAX_IDENT_LENGTH)
-        {
-          printf("Err: number too long\n");
-          return 0;
-        }
-        // copying array into buffer
-        k = i;
-        for (x = 0; x < length; x++)
-        {
-          buffer[x] = str[k++];
-        }
-        str[k] = '\0';
-        l = createLexeme(identsym, buffer);
-        list[j++] = *l;
-      }
-      else
-      {
-        printf("Err: invalid identifier\n");
-        return 0;
-      }
-
-      // capture identifier value
-      i = k;
-      while (!isspace(str[i++]));
-      k = i;
-      while (isdigit(str[k++]));
-      length = k - i;
-      if (length > MAX_NUM_LENGTH)
-      {
-        printf("Err: number too long\n");
-        return 0;
-      }
-      for (x = 0; x < length; x++)
-      {
-        buffer[x] = str[i++];
-      }
-      l = createLexeme(numbersym, buffer);
-      list[j++] = *l;
-    }
-
-    if(str[i] == 'v' && str[i+1] == 'a' && str[i+2]=='r')
-    {
-      //printf("var\n");
-      l = createLexeme(varsym, "var");
-      list[j++] = *l;
-
-      i+= 3;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-
-      // capture identifier (and make sure it's valid)
-      if (isalpha(str[i]))
-      {
-        // capture identifier as substring and put in list
-        i = k;
-        while (!symboltable(str[k++]))
-        {
-          if (!isspace(str[k]))
-          {
-            length++;
-          }
-        }
-        if ((length) > MAX_IDENT_LENGTH)
-        {
-          printf("Err: number too long\n");
-          return 0;
-        }
-        // copying array into buffer
-        k = i;
-        for (x = 0; x < length; x++)
-        {
-          buffer[x] = str[k++];
-        }
-        str[k] = '\0';
-        l = createLexeme(identsym, buffer);
-        list[j++] = *l;
-      }
-      else
-      {
-        printf("Err: invalid identifier\n");
-        return 0;
-      }
-    }
-
-    if(str[i] == 'p' && str[i+1] == 'r' && str[i+2]=='o' && str[i+3] == 'c' &&
-      str[i+4] == 'e' | str[i+5] == 'd' && str[i+6]=='u' && str[i+7] == 'r'
-      && str[i+8] == 'e' )
-    {
-      printf("procedure\n");
-      l = createLexeme(procsym, "procedure");
-      list[j++] = *l;
-
-      i+= 9;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-
-      while( str[i] != ';')
-      {
-
-      }
-      // return true;
-    }
-
-    if(str[i] == 'c' && str[i+1] == 'a' && str[i+2]=='l' && str[i+3] == 'l')
-    {
-      printf("call\n");
-      l = createLexeme( callsym, "call");
-      list[j++] = *l;
-
-      i+= 4;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(str[i] == 'b' && str[i+1] == 'e' && str[i+2]=='g' && str[i+3] == 'i' && str[i+4] == 'n')
-    {
-      printf("begin\n");
-      l = createLexeme( beginsym, "begin");
-      list[j++] = *l;
-
-      i+= 5;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(str[i] == 'e' && str[i+1] == 'n' && str[i+2]=='d')
-    {
-      printf("end\n");
-      l = createLexeme( endsym, "end");
-      list[j++] = *l;
-
-      i+= 3;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(str[i] == 'i' && str[i+1] == 'f' )
-    {
-      printf("if\n");
-      l = createLexeme( ifsym, "if");
-      list[j++] = *l;
-
-      i+= 2;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(str[i] == 't' && str[i+1] == 'h' && str[i+2]=='e' && str[i+3] == 'n')
-    {
-      l = createLexeme( thensym, "then");
-      list[j++] = *l;
-
-      i+= 4;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-
-    }
-
-    if(str[i] == 'e' && str[i+1] == 'l' && str[i+2]=='s' && str[i+3] == 'e')
-    {
-      printf("else\n");
-      l = createLexeme( elsesym, "else");
-      list[j++] = *l;
-
-      i+= 4;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(str[i] == 'w' && str[i+1] == 'h' && str[i+2]=='i' && str[i+3] == 'l' && str[i+4] == 'e')
-    {
-      printf("while\n");
-      l = createLexeme( whilesym, "while");
-      list[j++] = *l;
-
-      i+= 5;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(str[i] == 'd' && str[i+1] == 'o')
-    {
-      printf("do\n");
-      l = createLexeme( dosym, "do");
-      list[j++] = *l;
-
-      i+= 2;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(str[i] == 'r' && str[i+1] == 'e' && str[i+2]=='a' && str[i+3] == 'd')
-    {
-      printf("read\n");
-      l = createLexeme( readsym, "read");
-      list[j++] = *l;
-
-      i+= 4;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(str[i] == 'w' && str[i+1] == 'r' && str[i+2]=='i' && str[i+3] == 't' && str[i+4] == 'e')
-    {
-      printf("write\n");
-      l = createLexeme( writesym, "write");
-      list[j++] = *l;
-
-      i+= 5;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(str[i] == 'o' && str[i+1] == 'd' && str[i+2]=='d')
-    {
-      printf("odd\n");
-      l = createLexeme( oddsym, "odd");
-      list[j++] = *l;
-
-      i+= 3;
-      while(isspace(str[i]))
-      {
-        i++;
-      }
-      // return true;
-    }
-
-    if(symboltable(str[i]))
-    {
-      if (str[i] == '\0')
-      {
-        l = createLexeme(nulsym, "\0");
-        list[j++] = *l;
-      }
-      if (str[i] == '+')
-      {
-        l = createLexeme(plussym, "+");
-        list[j++] = *l;
-      }
-      if (str[i] == '-')
-      {
-        l = createLexeme(minussym, "-");
-        list[j++] = *l;
-      }
-      if (str[i] == '*')
-      {
-        l = createLexeme(multsym, "*");
-        list[j++] = *l;
-      }
-      if (str[i] == '/')
-      {
-        l = createLexeme(slashsym, "/");
-        list[j++] = *l;
-      }
-      if (str[i] == ':')
-      {
-        if (str[i + 1] == '=')
-        {
-          l = createLexeme(eqlsym, ":=");
-          list[j++] = *l;
-        }
-      }
-      if (str[i] == '!')
-      {
-        if (str[i + 1] == '=')
-        {
-          l = createLexeme(neqsym, "!=");
-          list[j++] = *l;
-        }
-      }
-      if (str[i] == '<')
-      {
-        if (str[i + 1] == '=')
-        {
-          l = createLexeme(leqsym, "<=");
-          list[j++] = *l;
-        }
-        else
-        {
-          l = createLexeme(lessym, "<");
-          list[j++] = *l;
-        }
-      }
-      if (str[i] == '>')
-      {
-        if (str[i + 1] == '=')
-        {
-          l = createLexeme(geqsym, ">=");
-          list[j++] = *l;
-        }
-        else
-        {
-          l = createLexeme(gtrsym, ">");
-        }
-      }
-      if (str[i] == ',')
-      {
-        l = createLexeme(commasym, ",");
-        list[j++] = *l;
-      }
-      if (str[i] == ';')
-      {
-        l = createLexeme(semicolonsym, ";");
-        list[j++] = *l;
-      }
+      return true;
     }
   }
-  return 1;
-}
-
-bool symboltable(char symbol)
-{
-  char validsymbols[13] = {'+', '-', '*', '/', '(', ')', '=', ',', '.', '<', '>',  ';', ':'};
-
-  for(int i=0; i<13; i++)
+  if (str[0] == 'c')
   {
-    if(symbol == validsymbols[i])
+    if (strcmp(reserved[0], str) == 0)
     {
-      return 1;
+      return true;
+    }
+    else if (strcmp(reserved[3], str) == 0)
+    {
+      return true;
     }
   }
-
-  return 0;
+  if (str[0] == 'd')
+  {
+    if (strcmp(reserved[10], str) == 0)
+    {
+      return true;
+    }
+  }
+  if (str[0] == 'e')
+  {
+    if (strcmp(reserved[5], str) == 0)
+    {
+      return true;
+    }
+    else if (strcmp(reserved[8], str) == 0)
+    {
+      return true;
+    }
+  }
+  if (str[0] == 'i')
+  {
+    if (strcmp(reserved[6], str) == 0)
+    {
+      return true;
+    }
+  }
+  if (str[0] == 'o')
+  {
+    if (strcmp(reserved[13], str) == 0)
+    {
+      return true;
+    }
+  }
+  if (str[0] == 'p')
+  {
+    if (strcmp(reserved[2], str) == 0)
+    {
+      return true;
+    }
+  }
+  if (str[0] == 'r')
+  {
+    if (strcmp(reserved[11], str) == 0)
+    {
+      return true;
+    }
+  }
+  if (str[0] == 't')
+  {
+    if (strcmp(reserved[7], str) == 0)
+    {
+      return true;
+    }
+  }
+  if (str[0] == 'v')
+  {
+    if (strcmp(reserved[1], str) == 0)
+    {
+      return true;
+    }
+  }
+  if (str[0] == 'w')
+  {
+    if (strcmp(reserved[9], str) == 0)
+    {
+      return true;
+    }
+    else if (strcmp(reserved[12], str) == 0)
+    {
+      return true;
+    }
+  }
+  return false;
 }
 
 int main(int argc, char **argv)
 {
-  char c, source[MAX_CODE_LENGTH];
-  lexeme list[MAX_CODE_LENGTH];
-  FILE *fp = fopen(argv[1], "r");
-  int i = 0;
+  FILE *fp;
+  fp = fopen(argv[1], "r");
+  char aSingleLine[MAX_CODE_LENGTH], code[MAX_CODE_LENGTH] = {'\0'};
 
-  fscanf(fp, "%c", &c);
-  while (!feof(fp))
+  while(!feof(fp))
   {
-    source[i++] = c;
-    fscanf(fp, "%c", &c);
+    fgets(aSingleLine, MAX_CODE_LENGTH, fp);
+    strcat(code, aSingleLine);
   }
+  printf("Input:\n%s\n", code);
+
+  trim(code);
+
   fclose(fp);
-
-  printf("Source Program:\n%s\n", source);
-
-  trim(source);
-  parse(source, list);
-
   return 0;
 }
